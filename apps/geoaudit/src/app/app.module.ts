598199c33cfc92d13606aeeee0be8b5a8d8f768f
Michael Stokes <michaelstokes93@protonmail.com>
2021-05-05 19:04:00 +0100

    #1 - How To Build Maps in Angular with Leaflet, Part 2: The Marker Service

--- apps/geoaudit/src/app/app.module.ts
+++ apps/geoaudit/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { MarkerService } from './services/marker.service';
 
-@NgModule({
+@NgModule({ 
   declarations: [AppComponent, MapComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [MarkerService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- apps/geoaudit/src/app/map/map.component.ts
+++ apps/geoaudit/src/app/map/map.component.ts
@@ -1,6 +1,23 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 
+import { MarkerService } from '../services/marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'geoaudit-map',
   templateUrl: './map.component.html',
@@ -8,10 +25,12 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private markerService: MarkerService) { }
 
   ngAfterViewInit(): void {
     this.initMap();
+    // this.markerService.makeCapitalMarkers(this.map);
+    this.markerService.makeCapitalCircleMarkers(this.map);
   }
 
   private map;
