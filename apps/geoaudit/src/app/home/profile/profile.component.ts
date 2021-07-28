0d335667fb26d947f555227673316547f5e0e20d
Michael Stokes <michaelstokes93@protonmail.com>
2021-07-28 23:09:18 +0100

    #6 - Working on changing home address

--- apps/geoaudit/src/app/home/profile/profile.component.ts
+++ apps/geoaudit/src/app/home/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'apps/geoaudit/src/environments/environment';
 import { combineLatest, noop, Observable, of } from 'rxjs';
+import { MapsAPILoader } from '@agm/core';
 import { catchError, map, tap } from 'rxjs/operators';
 import { UserEntityService } from '../../entity-services/user-entity.service';
 import { Auth, User } from '../../models';
@@ -12,7 +13,7 @@ import { AlertService, AuthService } from '../../services';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
   step = null;
 
@@ -24,13 +25,28 @@ export class ProfileComponent implements OnInit {
 
   changePasswordStep = null;
 
+  changeAddressStep = null;
+
+  changeAddressForm: FormGroup;
+
+  geometry: {
+    lat: number,
+    lng: number
+  }
+
   user$ = this.userEntityService.getByKey(this.authService.authValue.user.id);
+  
+  homeAddressSearchCtrl = new FormControl();
+
+  @ViewChild('homeAddressSearchInput') homeAddressSearchInput: ElementRef;
 
   constructor(
     private alertService: AlertService,
     private authService: AuthService,
     private formBuilder: FormBuilder,
-    private userEntityService: UserEntityService
+    private userEntityService: UserEntityService,
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
   ) {
     this.API_URL = environment.API_URL;
   }
@@ -46,9 +62,47 @@ export class ProfileComponent implements OnInit {
       passwordConfirmation: ['', Validators.required]
     })
 
+    this.changeAddressForm = this.formBuilder.group({
+      home: null,
+      work: null
+    })
+
     this.user$
       .pipe(
         map((user) => {
+
+          // const latlng = new google.maps.LatLng(user.home.lat, user.home.lng)
+
+          // console.log('latlng', latlng)
+
+          this.mapsAPILoader.load().then(() => {
+
+          this.ngZone.run(() => {
+            const latlng = new google.maps.LatLng(user.home.lat, user.home.lng)
+
+            const geocoder = new google.maps.Geocoder();
+  
+            geocoder.geocode({ location: latlng }, (results, status) => {
+              if (status !== google.maps.GeocoderStatus.OK) {
+                alert(status);
+            }
+            // This is checking to see if the Geoeode Status is OK before proceeding
+            if (status == google.maps.GeocoderStatus.OK) {
+                console.log(results);
+                var address = (results[0].formatted_address);
+
+                console.log('address', address)
+  
+                this.changeAddressForm.patchValue({
+                  home: address
+                })
+
+                this.homeAddressSearchCtrl.patchValue(address)
+              }
+            })
+          })
+          })
+
           this.changeUsernameForm.patchValue({
             username: user.username,
           });
@@ -57,10 +111,54 @@ export class ProfileComponent implements OnInit {
       .subscribe();
   }
 
-  setStep(index: number, accordion: 'CHANGE_PASSWORD') {
+  ngAfterViewInit() {
+
+    this.homeAddressSearchCtrl.valueChanges.subscribe((value) => {
+      const place = this.place(this.homeAddressSearchInput, 'home');
+
+      console.log('place', place, this.geometry)
+    })
+  }
+
+  place(input: ElementRef, attribute: 'home' | 'work') {
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(
+        input.nativeElement
+      );
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          const geometry = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+          }
+          
+          this.geometry = geometry;
+          
+          this.updateUser({
+            id: this.authService.authValue.user.id,
+            [attribute]: geometry
+          })
+        });
+      });
+    });
+  }
+
+  setStep(index: number, accordion: 'CHANGE_PASSWORD' | 'CHANGE_ADDRESS') {
     switch (accordion) {
       case 'CHANGE_PASSWORD':
         this.changePasswordStep = index;
+      break;
+
+      case 'CHANGE_ADDRESS':
+        this.changeAddressStep = index;
       break;
     }
   }
