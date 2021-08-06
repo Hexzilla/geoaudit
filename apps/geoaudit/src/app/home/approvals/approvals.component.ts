04e13d46cedec86dddb710563af6ac8b279b4466
Michael Stokes <michaelstokes93@protonmail.com>
2021-08-06 20:30:40 +0100

    #115 - approve

--- apps/geoaudit/src/app/home/approvals/approvals.component.ts
+++ apps/geoaudit/src/app/home/approvals/approvals.component.ts
@@ -2,57 +2,116 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import qs from 'qs';
+import { StatusEntityService } from '../../entity-services/status-entity.service';
 import { SurveyEntityService } from '../../entity-services/survey-entity.service';
-import { Statuses, Survey } from '../../models';
+import { Status, Statuses, Survey } from '../../models';
+import { AuthService } from '../../services';
 
 @Component({
   selector: 'geoaudit-approvals',
   templateUrl: './approvals.component.html',
-  styleUrls: ['./approvals.component.scss']
+  styleUrls: ['./approvals.component.scss'],
 })
 export class ApprovalsComponent implements OnInit {
-
   displayedColumns: Array<String> = [
     'select',
     'reference',
     'name',
     'date_delivery',
     'conducted_by',
-    'job'
+    'job',
   ];
 
   selection = new SelectionModel<Survey>(true, []);
 
   dataSource: MatTableDataSource<Survey>;
 
+  refusedStatus: Status;
+
   constructor(
+    private authService: AuthService,
+    private statusEntityService: StatusEntityService,
     private surveyEntityService: SurveyEntityService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    const parameters = qs.stringify({
-      _where: {
-        // "footer.approved": false,
-        "status.name": Statuses.COMPLETED
-      },
-      _sort: 'created_at:DESC'
-    })
+    this.query();
 
-    this.surveyEntityService.getWithQuery(parameters).subscribe(
-      (surveys) => {
-        this.dataSource = new MatTableDataSource(surveys.filter(survey => !survey?.footer?.approved));
+    this.statusEntityService.getAll().subscribe(
+      (statuses) => {
+        this.refusedStatus = statuses.find(status => status.name === Statuses.REFUSED);
       },
 
       (err) => {}
     )
   }
 
-  approve() {}
+  query() {
+    const parameters = qs.stringify({
+      _where: {
+        // "footer.approved": false,
+        'status.name': Statuses.COMPLETED,
+      },
+      _sort: 'created_at:DESC',
+    });
 
-  disapprove() {}
+    this.surveyEntityService.getWithQuery(parameters).subscribe(
+      (surveys) => {
+        this.dataSource = new MatTableDataSource(
+          surveys.filter((survey) => !survey?.footer?.approved)
+        );
+      },
+
+      (err) => {}
+    );
+  }
+
+  approve() {
+    this.selection.selected.map((survey) => {
+      this.surveyEntityService
+        .update({
+          ...survey,
+          footer: {
+            ...survey?.footer,
+            approved: true,
+            approved_by: this.authService.authValue.user,
+          },
+        })
+        .subscribe(
+          (update) => {
+            console.log('update', update);
+            this.query();
+          },
+
+          (err) => {}
+        );
+    });
+  }
+
+  disapprove() {
+    this.selection.selected.map((survey) => {
+      this.surveyEntityService
+        .update({
+          ...survey,
+          status: this.refusedStatus,
+          footer: {
+            ...survey?.footer,
+            approved: false,
+          },
+        })
+        .subscribe(
+          (update) => {
+            console.log('update', update);
+            this.query();
+          },
+
+          (err) => {}
+        );
+    });
+  }
 
   print() {}
-  
+
   download() {}
 
   isAllSelected() {
