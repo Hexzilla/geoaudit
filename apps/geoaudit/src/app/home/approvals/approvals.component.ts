f5393efda8ce4eaa8553746dd09672b2bfe0179d
Michael Stokes <michaelstokes93@protonmail.com>
2021-08-07 13:44:46 +0100

    #115 - Print and Download

--- apps/geoaudit/src/app/home/approvals/approvals.component.ts
+++ apps/geoaudit/src/app/home/approvals/approvals.component.ts
@@ -1,13 +1,17 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import qs from 'qs';
+import autoTable from 'jspdf-autotable';
+import Papa from 'papaparse';
 import { StatusEntityService } from '../../entity-services/status-entity.service';
 import { SurveyEntityService } from '../../entity-services/survey-entity.service';
 import { RefusalModalComponent } from '../../modals/refusal-modal/refusal-modal.component';
 import { Status, Statuses, Survey } from '../../models';
 import { AuthService } from '../../services';
+import * as moment from 'moment';
 
 @Component({
   selector: 'geoaudit-approvals',
@@ -23,6 +27,8 @@ export class ApprovalsComponent implements OnInit {
     'conducted_by',
     'job',
   ];
+
+  fields = ['ID Reference', 'Name', 'Delivery Date', 'Conducted By', 'Job'];
 
   selection = new SelectionModel<Survey>(true, []);
 
@@ -42,11 +48,13 @@ export class ApprovalsComponent implements OnInit {
 
     this.statusEntityService.getAll().subscribe(
       (statuses) => {
-        this.refusedStatus = statuses.find(status => status.name === Statuses.REFUSED);
+        this.refusedStatus = statuses.find(
+          (status) => status.name === Statuses.REFUSED
+        );
       },
 
       (err) => {}
-    )
+    );
   }
 
   query() {
@@ -92,11 +100,10 @@ export class ApprovalsComponent implements OnInit {
   }
 
   disapprove() {
-
     const dialogRef = this.dialog.open(RefusalModalComponent, {
       data: {
-        surveys: this.selection.selected
-      }
+        surveys: this.selection.selected,
+      },
     });
 
     dialogRef.afterClosed().subscribe(() => {
@@ -115,16 +122,80 @@ export class ApprovalsComponent implements OnInit {
               console.log('update', update);
               this.query();
             },
-  
+
             (err) => {}
           );
       });
     });
   }
 
-  print() {}
+  getBody(): Array<any> {
+    /**
+     *
+     */
+    const surveys = this.selection.isEmpty()
+      ? this.dataSource.data
+      : this.selection.selected;
 
-  download() {}
+    return surveys.map((survey) => {
+      return [
+        survey.reference ? survey.reference : '',
+        survey.name ? survey.name : '',
+        survey.date_delivery ? moment(survey.date_delivery).format('L LT') : '',
+        survey.conducted_by && survey.conducted_by.username
+          ? survey.conducted_by.username
+          : '',
+        survey.job.reference ? survey.job.reference : '',
+      ];
+    });
+  }
+
+  print() {
+    this.pdf(true);
+  }
+
+  download(): void {
+    this.pdf();
+    this.csv();
+  }
+
+  pdf(print = false) {
+    const doc = new jsPDF({
+      orientation: 'landscape',
+    });
+    // var finalY = doc.lastAutoTable.finalY || 10
+
+    doc.setFontSize(40);
+    doc.text(`Surveys`, 35, 25);
+
+    autoTable(doc, {
+      startY: 35,
+      head: [this.fields],
+      body: this.getBody(),
+    });
+
+    if (print) {
+      doc.autoPrint();
+      doc.output('dataurlnewwindow');
+    } else {
+      doc.save(`${moment().toISOString(true)}-survey-approval-list-download.pdf`);
+    }
+  }
+
+  csv() {
+    const csv = Papa.unparse({
+      data: this.getBody(),
+      fields: this.fields,
+    });
+    const blob = new Blob([csv]);
+
+    var a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(blob);
+    a.download = `${moment().toISOString(true)}-survey-download.csv`;
+    document.body.appendChild(a);
+    a.click(); // IE: "Access is denied"; see: https://connect.microsoft.com/IE/feedback/details/797361/ie-10-treats-blob-url-as-cross-origin-and-denies-access
+    document.body.removeChild(a);
+  }
 
   isAllSelected() {
     if (this.dataSource) {
