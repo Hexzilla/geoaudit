53e6b5ac9b9926740ddeca3d01658bcf4ef4b916
Michael Stokes <michaelstokes93@protonmail.com>
2021-08-22 10:13:13 +0100

    #52 - Added component survey action button and rendering buttons on survey component

---
+++ apps/geoaudit/src/app/components/survey-action-button/survey-action-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'geoaudit-survey-action-button',
+  templateUrl: './survey-action-button.component.html',
+  styleUrls: ['./survey-action-button.component.scss']
+})
+export class SurveyActionButtonComponent implements OnInit {
+
+  @Input() label;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
