07c7a7329f4c610fa5b0b0b070a92c6c9e5bd920
goldencat-tom <bluebird6922@gmail.com>
2021-09-02 10:51:40 +0300

    #124 - form tp-action component

--- apps/geoaudit/src/app/home/testposts/tp-action/tp-action.component.spec.ts
+++ apps/geoaudit/src/app/home/testposts/tp-action/tp-action.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestpostComponent } from './testpost.component';
+import { TpActionComponent } from './tp-action.component';
 
 describe('TestpostComponent', () => {
-  let component: TestpostComponent;
-  let fixture: ComponentFixture<TestpostComponent>;
+  let component: TpActionComponent;
+  let fixture: ComponentFixture<TpActionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TestpostComponent ]
+      declarations: [ TpActionComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TestpostComponent);
+    fixture = TestBed.createComponent(TpActionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- apps/geoaudit/src/app/home/testposts/tp-action/tp-action.component.ts
+++ apps/geoaudit/src/app/home/testposts/tp-action/tp-action.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ThemePalette } from '@angular/material/core';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
@@ -7,8 +13,22 @@ import * as MapActions from '../../../store/map/map.actions';
 import * as fromApp from '../../../store';
 import * as moment from 'moment';
 import { TestpostEntityService } from '../../../entity-services/testpost-entity.service';
-import { Abriox, Conditions, Image, MarkerColours, Survey, Testpost, TpAction } from '../../../models';
-import { debounceTime, tap, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import {
+  Abriox,
+  Conditions,
+  Image,
+  MarkerColours,
+  Survey,
+  Testpost,
+  TpAction,
+  Status,
+} from '../../../models';
+import {
+  debounceTime,
+  tap,
+  distinctUntilChanged,
+  takeUntil,
+} from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { AlertService } from '../../../services';
 import { FileTypes } from '../../../components/file-upload/file-upload.component';
@@ -22,49 +42,25 @@ import { SurveyEntityService } from '../../../entity-services/survey-entity.serv
 @Component({
   selector: 'geoaudit-tp-action',
   templateUrl: './tp-action.component.html',
-  styleUrls: ['./tp-action.component.scss']
+  styleUrls: ['./tp-action.component.scss'],
 })
 export class TpActionComponent implements OnInit, AfterViewInit {
-
-  id: string;
-
+  /**
+   * The form consisting of the form fields.
+   */
   form: FormGroup;
 
-  color: ThemePalette = 'primary';
+  /**
+   * An array of status i.e. NOT_STARTED, ONGOING, etc.
+   */
+  statuses: Array<Status>;
 
-  @ViewChild('dateInstallationDateTimePicker') dateInstallationDateTimePicker: any;
+  /**
+   * The current job state.
+   */
+  currentState = 0;
 
-  @ViewChild('latCtrlInput') latCtrlInput: ElementRef;
-  @ViewChild('lngCtrlInput') lngCtrlInput: ElementRef;
-
-  latCtrl = new FormControl();
-
-  lngCtrl = new FormControl();
-
-  public disabled = false;
-  public showSpinners = true;
-  public showSeconds = false;
-  public touchUi = false;
-  public enableMeridian = false;
-  public minDate: Date;
-  public maxDate: Date;
-  public stepHour = 1;
-  public stepMinute = 1;
-  public stepSecond = 1;
-  public disableMinute = false;
-  public hideTime = false;
-
-  private unsubscribe = new Subject<void>();
-
-  API_URL: string;
-
-  abriox: Abriox;
-
-  testpost: Testpost;
-
-  tp_actions: Array<TpAction> = [];
-
-  // surveys: Array<Survey> = [];
+  public selectedTabIndex = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -77,327 +73,56 @@ export class TpActionComponent implements OnInit, AfterViewInit {
     private alertService: AlertService,
     private _lightbox: Lightbox,
     private dialog: MatDialog
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.API_URL = environment.API_URL;
-
-    this.id = this.route.snapshot.paramMap.get('id');
-
-    this.initForm();
-
-    if (this.id) {
-      this.getTestpostAndPatchForm(this.id);
-    } else {
-      // this.createMode();
-    }
-
-    this.store.select('map').subscribe((map) => {
-      if (map.clickMarker) {
-        this.latCtrl.setValue(map.clickMarker.lat);
-        this.lngCtrl.setValue(map.clickMarker.lng);
-      }
-    });
-  }
-
-  ngAfterViewInit() {
-    this.latCtrl.valueChanges.subscribe((value) => {
-      if (value) {
-        this.form.patchValue({
-          geometry: {
-            ...this.form.value.geometry,
-            lat: value,
-          },
-        });
-      }
-    });
-
-    this.lngCtrl.valueChanges.subscribe((value) => {
-      if (value) {
-        this.form.patchValue({
-          geometry: {
-            ...this.form.value.geometry,
-            lng: value,
-          },
-        });
-      }
-    });
-  }
-
-  initForm() {
-    this.form = this.formBuilder.group({
-
-      reference: [''],
-      name: [''],
-      date_installation: [moment().toISOString()],
-      manufacture: [''],
-      model: [''],
-      serial_number: [''],
-
-      geometry: [null],
-
-      images: [],
-      documents: [],
-
-
-      // datetime: moment().toISOString(),
-      // description: null,
-      // images: [[]],
-      // attachments: [[]],
-
-      // assignees: [],
-
-      // abrioxes: [],
-      // jobs: [],
-      // resistivities: [],
-      // sites: [],
-      // surveys: [],
-      // testposts: [],
-      // trs: [],
-
-      id: null, // Whilst we don't edit the id, we submit the form.
-    });
-  }
-
-  getTestpostAndPatchForm(id: string) {
-    this.testpostEntityService.getByKey(id).subscribe(
-      (testpost) => {
-        this.patchForm(testpost);
-
-        this.abriox = testpost.abriox;
-
-        testpost.tp_actions.map(tp_action => {
-          this.tpActionEntityService.getByKey(tp_action.id).subscribe(item => {
-            this.tp_actions.push(item);
-            
-            // this.surveyEntityService.getByKey(item.survey.id).subscribe(survey => {
-            //   this.surveys.push(survey);
-            // })
-          })
-        })
-
-        this.testpost = testpost;
-
-
-        this.autoSave(this.id ? false : true);
-      },
-
-      (err) => {}
-    )
-  }
-
-  autoSave(reload = false) {
-    this.form.valueChanges
-      .pipe(
-        debounceTime(500),
-        tap(() => {
-          this.submit(false);
-        }),
-        distinctUntilChanged(),
-        takeUntil(this.unsubscribe)
-      )
-      .subscribe(() => {
-        /**
-         * Workaround.
-         *
-         * When we navigate to the create a note component. We may have
-         * provided a jobs query parameter. However the jobs selector is not
-         * updated with that and therefore we're reloading such that it
-         * goes into edit mode.
-         */
-        if (reload) {
-          this.router
-            .navigate([`/home/testposts/${this.form.value.id}`])
-            .then(() => {
-              window.location.reload();
-            });
-        }
-      });
-  }
-
-  submit(navigate = true) {
-    // reset alerts on submit
-    this.alertService.clear();
-
-    if (this.form.invalid) {
-      this.alertService.error('Invalid');
-      return;
-    }
-
     /**
-     * Invoke the backend with a PUT request to update
-     * the job with the form values.
-     *
-     * If create then navigate to the job id.
+     * Initialise the form with properties and validation
+     * constraints.
      */
-    this.testpostEntityService.update(this.form.value).subscribe(
-      (update) => {
-        this.alertService.info('Saved Changes');
-
-        if (navigate) this.router.navigate([`/home/testposts`]);
-      },
-
-      (err) => {
-        this.alertService.error('Something went wrong');
-      },
-
-      () => {}
-    );
+    this.initialiseForm();
   }
 
-  patchForm(testpost: Testpost) {
-    const {
-      id,
+  ngAfterViewInit(): void {}
 
-      reference,
-      name,
-      date_installation,
-      manufacture,
-      model,
-      serial_number,
-      geometry,
-    
-      images,
-      documents
-    } = testpost;
-
-    this.form.patchValue({
-      id,
-
-      reference,
-      name,
-      date_installation,
-      manufacture,
-      model,
-      serial_number,
-      geometry,
-
-      images,
-      documents
-    })
-
-    this.latCtrl.setValue(geometry?.lat);
-    this.lngCtrl.setValue(geometry?.lng);
-  }
-
-  clickMarker(): void {
-    this.store.dispatch(
-      MapActions.toggleSidebar({
-        url: this.router.url,
-      })
-    );
-  }
-
-
-  onPreview(fileType: FileTypes): void {
-    const { images, documents } = this.form.value;
-
-    switch (fileType) {
-      case FileTypes.IMAGE:
-        let _album: Array<IAlbum> = [];
-
-        images.map((image: Image) => {
-          const album = {
-            src: `${this.API_URL}${image.url}`,
-            caption: image.name,
-            thumb: `${this.API_URL}${image.formats.thumbnail.url}`,
-          };
-
-          _album.push(album);
-        });
-
-        if (_album.length >= 1) this._lightbox.open(_album, 0);
-        break;
-
-      case FileTypes.DOCUMENT:
-        const dialogRef = this.dialog.open(AttachmentModalComponent, {
-          data: {
-            fileType,
-            documents,
-          },
-        });
-
-        dialogRef.afterClosed().subscribe((result) => {});
-        break;
-    }
-  }
-
-  onImageUpload(event): void {
-    const { images } = this.form.value;
-
-    // this.images.push(event)
-
-    // May be multiple so just preserving the previous object on the array of images
-
-    this.form.patchValue({
-      images: [...images, event],
+  /**
+   * Initialisation of the form, properties, and validation.
+   */
+  initialiseForm(): void {
+    this.form = this.formBuilder.group({
+      date: null,
+      condition: null,
+      pipe_on: null,
+      pipe_off: null,
+      anodes_on: null,
+      anodes_off: null,
+      anodes_current: null,
+      dead_on: null,
+      dead_off: null,
+      dead_current: null,
+      sleeve_on: null,
+      sleeve_off: null,
+      sleeve_current: null,
+      coupon_on: null,
+      coupon_off: null,
+      coupon_current_ac: null,
+      coupon_current_dc: null,
+      cd_input_v: null,
+      cd_input_a: null,
+      cd_output_v: null,
+      cd_output_a: null,
+      assert_pipe_depth: null,
+      assert_reinstatement: null,
+      fault_type: null,
+      fault_desc: null,
     });
-
-    this.submit(false);
   }
 
-  onDocumentUpload(event): void {
-    const { documents } = this.form.value;
-
-    this.form.patchValue({
-      documents: [...documents, event],
-    });
-
-    this.submit(false);
+  submit() {
+    console.log("submit")
   }
 
-  addAbriox() {
-    
-    const navigationExtras: NavigationExtras = {
-      queryParams: {
-        testpost: this.form.value.id
-      }
-    }
-
-    this.router.navigate(["/home/abrioxes/create"], navigationExtras)
+  selectedIndexChange(selectedTabIndex) {
+    this.selectedTabIndex = selectedTabIndex;
   }
-
-  getConditionColour(id?: number) {
-
-    let color = "00FFFFFF";
-
-    if (id) {
-        if (this.tp_actions) {
-          const tp_action = this.tp_actions.find(tp_action => tp_action.condition.id === id);
-
-          if (tp_action) {
-            color = MarkerColours[tp_action.condition.name];
-          }
-        }   
-    }
-
-    return color;
-  }
-
-  getLatestConditionColour() {
-    let color = "00FFFFFF";
-
-    if (this.tp_actions) {
-      const tp_action = this.tp_actions.sort((a: any, b: any) => b.date - a.date);
-
-      if (tp_action) {
-        color = MarkerColours[tp_action[tp_action.length - 1]?.condition.name];
-      }
-    }  
-
-    return color;
-  }
-
-  // getSurvey(id?: number) {
-  //   let survey: Survey;
-
-
-  //   if (id) {
-  //     if (this.surveys) {
-  //       survey = this.surveys.find(item => item.id === id);
-  //     }
-  //   }
-
-  //   return survey;
-  // }
 }
