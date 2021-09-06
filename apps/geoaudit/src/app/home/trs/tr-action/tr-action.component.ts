dae93f567ea4cda445e6f48c7bcbe637213374a9
goldencat-tom <bluebird6922@gmail.com>
2021-09-06 10:24:36 +0300

    #122 - update fault, attachments

--- apps/geoaudit/src/app/home/trs/tr-action/tr-action.component.ts
+++ apps/geoaudit/src/app/home/trs/tr-action/tr-action.component.ts
@@ -5,39 +5,24 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ThemePalette } from '@angular/material/core';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import * as MapActions from '../../../store/map/map.actions';
 import * as fromApp from '../../../store';
 import * as moment from 'moment';
 import { TestpostEntityService } from '../../../entity-services/testpost-entity.service';
 import {
-  Abriox,
-  Conditions,
-  Image,
-  MarkerColours,
-  Survey,
   Testpost,
   TrAction,
   Status,
 } from '../../../models';
-import {
-  debounceTime,
-  tap,
-  distinctUntilChanged,
-  takeUntil,
-} from 'rxjs/operators';
-import { fromEvent, Subject } from 'rxjs';
-import { AlertService } from '../../../services';
+import { AlertService, UploadService } from '../../../services';
 import { FileTypes } from '../../../components/file-upload/file-upload.component';
 import { IAlbum, Lightbox } from 'ngx-lightbox';
-import { AttachmentModalComponent } from '../../../modals/attachment-modal/attachment-modal.component';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'apps/geoaudit/src/environments/environment';
 import { StatusEntityService } from '../../../entity-services/status-entity.service';
-import { TpActionEntityService } from '../../../entity-services/tp-action-entity.service';
+import { TrActionEntityService } from '../../../entity-services/tr-action-entity.service';
 import { SurveyEntityService } from '../../../entity-services/survey-entity.service';
 
 @Component({
@@ -66,9 +51,15 @@ export class TrActionComponent implements OnInit, AfterViewInit {
    */
   submitted = false;
 
+
+  actionId = 0;
+
   public tr_action: TrAction = null;
 
   public selectedTabIndex = 0;
+  
+  attachedImages: Array<any>;
+  attachedDocuments: Array<any>;
 
   constructor(
     private route: ActivatedRoute,
@@ -76,10 +67,11 @@ export class TrActionComponent implements OnInit, AfterViewInit {
     private surveyEntityService: SurveyEntityService,
     private testpostEntityService: TestpostEntityService,
     private statusEntityService: StatusEntityService,
-    private tpActionEntityService: TpActionEntityService,
+    private trActionEntityService: TrActionEntityService,
     private store: Store<fromApp.State>,
     private router: Router,
     private alertService: AlertService,
+    private uploadService: UploadService,
     private _lightbox: Lightbox,
     private dialog: MatDialog
   ) {}
@@ -98,9 +90,83 @@ export class TrActionComponent implements OnInit, AfterViewInit {
      * constraints.
      */
     this.initialiseForm();
+
+    this.fetchTpAction();
   }
 
   ngAfterViewInit(): void {
+    //
+  }
+
+  fetchTpAction() {
+    this.actionId = this.route.snapshot.params['actionId'];
+    console.log("actionId", this.actionId)
+
+    this.trActionEntityService.getByKey(this.actionId).subscribe(
+      (tr_action) => {
+        this.tr_action = tr_action;
+
+        //TODO - FormArray
+        if (tr_action) {
+          this.form.patchValue({
+            date: moment(tr_action.date).format('L LT'),
+            condition: tr_action.condition?.name,
+
+            images: tr_action.images,
+            documents: tr_action.documents
+          });
+        }
+      },
+      (err) => {}
+    )
+  }
+
+  get anodes(): FormArray {
+    return this.form.get('anodes') as FormArray;
+  }
+
+  addAnode(anode) {
+    if (anode) {
+		  this.anodes.push(this.formBuilder.group(anode));
+    } else {
+      this.anodes.push(this.formBuilder.group({
+        anodes_on: '',
+        anodes_off: '',
+        anodes_current: '',
+      }));
+    }
+  }
+
+  get currentDrains(): FormArray {
+    return this.form.get('currentDrains') as FormArray;
+  }
+
+  addCurrentDrain(drain) {
+    if (drain) {
+		  this.currentDrains.push(this.formBuilder.group(drain));
+    } else {
+      this.currentDrains.push(this.formBuilder.group({
+        cd_input_v: '',
+        cd_input_a: '',
+        cd_output_v: '',
+        cd_output_a: ''
+      }));
+    }
+  }
+  
+  get faults(): FormArray {
+    return this.form.get('faults') as FormArray;
+  }
+
+  addFaults(fault) {
+    if (fault) {
+		  this.faults.push(this.formBuilder.group(fault));
+    } else {
+      this.faults.push(this.formBuilder.group({
+        type: '',
+        desc: ''
+      }));
+    }
   }
 
   /**
@@ -113,14 +179,9 @@ export class TrActionComponent implements OnInit, AfterViewInit {
       reading_volt: null,
       reading_amps: null,
       current_setting: null,
-      anodes_off: null,
-      anodes_current: null,
-      cd_input_v: null,
-      cd_input_a: null,
-      cd_output_v: null,
-      cd_output_a: null,
-      fault_type: null,
-      fault_desc: null,
+      anodes: new FormArray([]),
+      currentDrains: new FormArray([]),
+      faults: new FormArray([]),
     });
   }
 
@@ -171,5 +232,42 @@ export class TrActionComponent implements OnInit, AfterViewInit {
     else if (e.refuse) {
       //TODO: update action state to refused
     }
+  }
+
+  onImageUpload(event): void {
+    const { images } = this.form.value;
+    this.form.patchValue({
+      images: [...images, event],
+    });
+
+    this.getUploadFiles();
+    this.submit(false);
+  }
+
+  onDocumentUpload(event): void {
+    const { documents } = this.form.value;
+
+    this.form.patchValue({
+      documents: [...documents, event],
+    });
+
+    this.getUploadFiles();
+    this.submit(false);
+  }
+
+  onPreview(fileType: FileTypes): void {
+    const { images, documents } = this.form.value;
+    this.uploadService.onPreview(fileType, images, documents);
+  }
+
+  onItemPreview(param: any): void {
+    const { images, documents } = this.form.value;
+    this.uploadService.onItemPreview(param.fileType, images, documents, param.index);
+  }
+
+  getUploadFiles(): void {
+    const { images, documents } = this.form.value;
+    this.attachedImages = this.uploadService.getImageUploadFiles(images);
+    this.attachedDocuments = this.uploadService.getDocumentUploadFiles(documents);
   }
 }
