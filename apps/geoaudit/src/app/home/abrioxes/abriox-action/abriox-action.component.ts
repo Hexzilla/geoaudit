c4f07207956da806c7b03c150e3d51a9c2a558fc
goldencat-tom <bluebird6922@gmail.com>
2021-09-08 05:40:15 +0300

    #143 - update abrix action component

--- apps/geoaudit/src/app/home/abrioxes/abriox-action/abriox-action.component.ts
+++ apps/geoaudit/src/app/home/abrioxes/abriox-action/abriox-action.component.ts
@@ -5,39 +5,24 @@ import {
   OnInit,
   ViewChild,
 } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { ThemePalette } from '@angular/material/core';
-import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import * as MapActions from '../../../store/map/map.actions';
 import * as fromApp from '../../../store';
 import * as moment from 'moment';
 import { TestpostEntityService } from '../../../entity-services/testpost-entity.service';
 import {
-  Abriox,
-  Conditions,
-  Image,
-  MarkerColours,
-  Survey,
   Testpost,
   AbrioxAction,
   Status,
 } from '../../../models';
-import {
-  debounceTime,
-  tap,
-  distinctUntilChanged,
-  takeUntil,
-} from 'rxjs/operators';
-import { fromEvent, Subject } from 'rxjs';
-import { AlertService } from '../../../services';
+import { AlertService, UploadService } from '../../../services';
 import { FileTypes } from '../../../components/file-upload/file-upload.component';
 import { IAlbum, Lightbox } from 'ngx-lightbox';
-import { AttachmentModalComponent } from '../../../modals/attachment-modal/attachment-modal.component';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'apps/geoaudit/src/environments/environment';
 import { StatusEntityService } from '../../../entity-services/status-entity.service';
-import { TpActionEntityService } from '../../../entity-services/tp-action-entity.service';
+import { AbrioxActionEntityService } from '../../../entity-services/abriox-action-entity.service';
 import { SurveyEntityService } from '../../../entity-services/survey-entity.service';
 
 @Component({
@@ -66,9 +51,16 @@ export class AbrioxActionComponent implements OnInit, AfterViewInit {
    */
   submitted = false;
 
+  abrioxId = 0;
+
+  actionId = 0;
+
   public abriox_action: AbrioxAction = null;
 
   public selectedTabIndex = 0;
+  
+  attachedImages: Array<any>;
+  attachedDocuments: Array<any>;
 
   constructor(
     private route: ActivatedRoute,
@@ -76,10 +68,11 @@ export class AbrioxActionComponent implements OnInit, AfterViewInit {
     private surveyEntityService: SurveyEntityService,
     private testpostEntityService: TestpostEntityService,
     private statusEntityService: StatusEntityService,
-    private tpActionEntityService: TpActionEntityService,
+    private abrioxActionEntityService: AbrioxActionEntityService,
     private store: Store<fromApp.State>,
     private router: Router,
     private alertService: AlertService,
+    private uploadService: UploadService,
     private _lightbox: Lightbox,
     private dialog: MatDialog
   ) {}
@@ -98,9 +91,57 @@ export class AbrioxActionComponent implements OnInit, AfterViewInit {
      * constraints.
      */
     this.initialiseForm();
+
+    this.fetchAbrioxAction();
   }
 
   ngAfterViewInit(): void {
+  }
+  fetchAbrioxAction() {
+    this.abrioxId = this.route.snapshot.params['id']
+    console.log("abrioxId", this.abrioxId)
+
+    this.actionId = this.route.snapshot.params['actionId'];
+    console.log("actionId", this.actionId)
+
+    this.abrioxActionEntityService.getByKey(this.actionId).subscribe(
+      (abriox_action) => {
+        this.abriox_action = abriox_action;
+        console.log("abriox_action", this.abriox_action)
+
+        //TODO - FormArray
+        if (abriox_action) {
+          this.form.patchValue({
+            date: moment(abriox_action.date).format('L LT'),
+            condition: abriox_action.condition?.name,
+
+            images: abriox_action.images,
+            documents: abriox_action.documents
+          });
+
+          //this.faults.clear();
+          //fault_detail.map(item => this.addFaults(null, item));
+          //console.log('this.faults', this.faults)
+        }
+      },
+      (err) => {}
+    )
+  }
+
+  get faults(): FormArray {
+    return this.form.get('faults') as FormArray;
+  }
+
+  addFaults(event, item=null) {
+    event?.preventDefault();
+    if (item) {
+		  this.faults.push(this.formBuilder.group(item));
+    } else {
+      this.faults.push(this.formBuilder.group({
+        type: '',
+        desc: ''
+      }));
+    }
   }
 
   /**
@@ -110,17 +151,7 @@ export class AbrioxActionComponent implements OnInit, AfterViewInit {
     this.form = this.formBuilder.group({
       date: null,
       condition: null,
-      reading_volt: null,
-      reading_amps: null,
-      current_setting: null,
-      anodes_off: null,
-      anodes_current: null,
-      cd_input_v: null,
-      cd_input_a: null,
-      cd_output_v: null,
-      cd_output_a: null,
-      fault_type: null,
-      fault_desc: null,
+      faults: new FormArray([]),
     });
   }
 
@@ -159,5 +190,65 @@ export class AbrioxActionComponent implements OnInit, AfterViewInit {
 
   selectedIndexChange(selectedTabIndex) {
     this.selectedTabIndex = selectedTabIndex;
+  }
+
+  getActionTitle() {
+    if (this.abriox_action) {
+      return `Abriox [${this.abriox_action.reference}]`;
+    }
+    return "Abriox";
+  }
+
+  searchAbriox() {
+    this.router.navigate([`/home/search`]);
+  }
+
+  updateMarkState(e) {
+    if (e.complete) {
+      //TODO: update action state to completed
+    }
+    else if (e.approve) {
+      //TODO: update action state to approved
+    }
+    else if (e.refuse) {
+      //TODO: update action state to refused
+    }
+  }
+
+  onImageUpload(event): void {
+    const { images } = this.form.value;
+    this.form.patchValue({
+      images: [...images, event],
+    });
+
+    this.getUploadFiles();
+    this.submit(false);
+  }
+
+  onDocumentUpload(event): void {
+    const { documents } = this.form.value;
+
+    this.form.patchValue({
+      documents: [...documents, event],
+    });
+
+    this.getUploadFiles();
+    this.submit(false);
+  }
+
+  onPreview(fileType: FileTypes): void {
+    const { images, documents } = this.form.value;
+    this.uploadService.onPreview(fileType, images, documents);
+  }
+
+  onItemPreview(param: any): void {
+    const { images, documents } = this.form.value;
+    this.uploadService.onItemPreview(param.fileType, images, documents, param.index);
+  }
+
+  getUploadFiles(): void {
+    const { images, documents } = this.form.value;
+    this.attachedImages = this.uploadService.getImageUploadFiles(images);
+    this.attachedDocuments = this.uploadService.getDocumentUploadFiles(documents);
   }
 }
